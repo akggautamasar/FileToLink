127ce6618e4e276b50295ce15a48d8dbfb098226
akggautamasar <Worksbeyondworks@gmail.com>
2025-04-15 20:32:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -24,7 +24,7 @@ URL = environ.get("URL", "")
 
 # Admins, Channels & Users
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002699534847'))
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6221765779').split()]
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://worksbeyondworks:QivSIc7kPVzdnizC@cluster0.iyjcjbb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
